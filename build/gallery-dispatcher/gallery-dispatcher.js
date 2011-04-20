77f2c48ce16ac4315353e72738694f9ea0f545fa
Tobias Ebnöther <ebi@gorn.ch>
2011-04-20 16:26:42 +0200

    Manually patched the Dispatcher to support IE9 and FF4 see https://github.com/blt04/yui3-gallery/blob/c0a25292a78a15a081394bdca51ee1beaf53092d/build/gallery-dispatcher/gallery-dispatcher.js

--- build/gallery-dispatcher/gallery-dispatcher.js
+++ build/gallery-dispatcher/gallery-dispatcher.js
@@ -174,7 +174,7 @@ Y.Dispatcher = Y.Base.create(DISPATCHER, Y.Base, [], {
         var d = ( jsNode ? jsNode.get('ownerDocument') : null ) || Y.one('doc'),
             h = d.one('head') || d.get('documentElement'),
             // creating a new script node to execute the inline javascrip code
-            newScript = Y.Node.create('<' + SC + '></' + SC + '>');
+            newScript = Y.one(document.createElement(SC));
 
         if (text) {
             newScript._node.text = text;
@@ -472,4 +472,4 @@ Y.Dispatcher = Y.Base.create(DISPATCHER, Y.Base, [], {
 });
 
 
-}, 'gallery-2011.03.02-20-58' ,{requires:['base', 'node-base', 'io-base', 'get', 'async-queue', 'classnamemanager']});
+}, '@VERSION@' ,{requires:['base', 'node-base', 'io-base', 'get', 'async-queue', 'classnamemanager']});
