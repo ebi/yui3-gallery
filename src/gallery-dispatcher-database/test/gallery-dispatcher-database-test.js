2673d6dec399877e0c8fb8c376acd4b431108858
Tobias Ebnöther <ebi@gorn.ch>
2011-11-22 16:09:03 +0100

    Added tests for _storeContent

--- src/gallery-dispatcher-database/test/gallery-dispatcher-database-test.js
+++ src/gallery-dispatcher-database/test/gallery-dispatcher-database-test.js
@@ -3,6 +3,12 @@ YUI({ base: '/HULLA'}).use('gallery-dispatcher-database', function (Y) {
 	//Shorthands
 	var assert = buster.assert,
 		disdb = Y.Dispatcher.database,
+		URL = 'someUrl',
+		RESPONSE = {
+			responseText: 'someResponse'
+		},
+		HOST = 'host',
+		CONTENT = 'content',
 
 	//Constants from gallery-dispatcher-database
 		ATTR_STORE = 'storeResponse',
@@ -11,6 +17,38 @@ YUI({ base: '/HULLA'}).use('gallery-dispatcher-database', function (Y) {
 
 	var allBrowsers = {
 		setUp: function () {
+			this.stub(disdb.prototype, 'initializer');
+			this.dbm = this.stub();
+			this.dbm.setItem = this.stub();
+			this.disdb = new disdb({ databaseManager: this.dbm });
+		},
+
+		"_storeContent": {
+			setUp: function () {
+				this.host = {
+					set: this.stub()
+				};
+				this.disdb.set(HOST, this.host);
+			},
+
+			"sets an item if store is true": function () {
+				this.disdb._storeContent(666, RESPONSE, {url: URL});
+
+				assert.calledOnce(this.dbm.setItem);
+				assert.calledWithExactly(this.dbm.setItem, URL, RESPONSE.responseText);
+				assert.calledOnce(this.host.set);
+				assert.calledWithExactly(this.host.set, CONTENT, RESPONSE.responseText);
+				
+			},
+
+			"passes to the host doesn't store": function () {
+				this.disdb.set(ATTR_STORE, false);
+				this.disdb._storeContent(666, RESPONSE, {url: URL});
+
+				assert.notCalled(this.dbm.setItem);
+				assert.calledOnce(this.host.set);
+				assert.calledWithExactly(this.host.set, CONTENT, RESPONSE.responseText);
+			}
 		}
 	};
 
@@ -19,26 +57,32 @@ YUI({ base: '/HULLA'}).use('gallery-dispatcher-database', function (Y) {
 			'database': !!window.openDatabase
 		},
 
-		"Initializer binds event": function () {
-			var hostStub = this.stub(disdb.prototype, 'onHostEvent');
+		initializer: {
+			setUp: function () {
+				disdb.prototype.initializer.restore();
+			},
 
-			(new disdb({databaseManager: this.stub()}));
+			"binds event": function () {
+				var hostStub = this.stub(disdb.prototype, 'onHostEvent');
 
-			assert.calledOnce(hostStub);
-		},
+				(new disdb({databaseManager: this.stub()}));
 
-		"Initializer creats a new DBM": function () {
-			var hostStub = this.stub(disdb.prototype, 'onHostEvent'),
-				setStub = this.stub(disdb.prototype, 'set'),
-				dbmStub = this.stub(Y, 'DatabaseManager');
+				assert.calledOnce(hostStub);
+			},
 
-			(new disdb());
+			"creats a new DBM": function () {
+				var hostStub = this.stub(disdb.prototype, 'onHostEvent'),
+					setStub = this.stub(disdb.prototype, 'set'),
+					dbmStub = this.stub(Y, 'DatabaseManager');
 
-			assert.calledOnce(hostStub);
-			assert.calledTwice(setStub); //One call comes from the initializer
-			assert.calledOnce(dbmStub);
-			assert.isTrue(dbmStub.calledWithNew());
-			assert.calledWithExactly(setStub, ATTR_DB, new dbmStub());
+				(new disdb());
+
+				assert.calledOnce(hostStub);
+				assert.calledTwice(setStub); //One call comes from the initializer
+				assert.calledOnce(dbmStub);
+				assert.isTrue(dbmStub.calledWithNew());
+				assert.calledWithExactly(setStub, ATTR_DB, new dbmStub());
+			}
 		}
 	};
 
